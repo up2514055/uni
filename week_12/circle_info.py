421f0e1c1c7074dff522c9262a48ff605d97535d
Dylan Mazur <up2514055@myport.ac.uk>
2026-03-16 10:08:07 +0000

    deleted duplicates

--- week_12/circle_info.py
+++
@@ -1,7 +0,0 @@
-import math
-
-def area_of_circle(radius):
-    return math.pi * radius ** 2
-
-def circumference_of_circle(radius):
-    return 2 * math.pi * radius
--- week_13/test.py
+++
@@ -1,8 +0,0 @@
-apply = lambda f, x: f(x)
-result = apply(lambda n: pow(n, 3), 4)
-print(result)
-
-
-loudify = lambda s: s.upper() + '!!!'
-alarm = lambda name: f'Wake up, {name}'
-double = lambda x: x * 2
