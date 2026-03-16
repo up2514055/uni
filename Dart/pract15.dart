90316f707f7003d194b72826691460c27f068958
Dylan Mazur <up2514055@myport.ac.uk>
2026-03-16 13:21:15 +0000

    gitignore

--- Dart/pract15.dart
+++ Dart/pract15.dart
@@ -1,14 +1,16 @@
 void main() {
-  print('Hello, World!');
-}
-
-void main_one() {
-  int x = 5;
-  int result = multiplyBy2(x);
-  print('Result: $result');
+  //int x = 5;
+  //int result = multiplyBy2(x);
+  //print('Result: $result');
   //the $ is the same as using {} in python when using f strings
 }
 
-int multiplyBy2(int, number) {
+int multiplyBy2(int number) {
   return number * 2;
 }
+
+void speedCalculator(double distance, double time) { 
+  double speed = distance / time; 
+  print('Speed: $speed'); 
+} 
+//double means float in python
